667acb2246d29519a753e50194f4c1e9f93f3562
Francisco das Chagas Silva Junior <fcsjr.chagas@gmail.com>
2023-08-30 21:09:20 -0400

    Add account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,28 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  host = "https://shrouded-atoll-27632.herokuapp.com"
+  config.action_mailer.default_url_options = { host: host }
+  # # config.action_mailer.delivery_method = :smtp
+  # ActionMailer::Base.smtp_settings = {
+  #   :address => "smtp.sendgrid.net",
+  #   :port => "587",
+  #   :authentication => :plain,
+  #   :user_name => "apikey"
+  #   :password => ENV["SENDGRID_API_KEY"],
+  #   :domain => "heroku.com",
+  #   :enable_starttls_auto => true
+  # }
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
